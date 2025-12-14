ba4b6e5bcee55291d547bd5f9cbc4f7d24268986
Mohmmad07861 <maknojiyamohmmad786@gmail.com>
2025-12-14 16:34:40 +0530

    Add files via upload

---
+++ Flow.py
@@ -0,0 +1,14 @@
+# 1 to 5 & 1
+for i in range(6,1,-1 ):  # 1,6
+    for j in range(1, i):   # (1,i+1)
+        print(j, end = ' ')
+    print()
+
+#pattern 02 
+for i in range(6,1,-1):             
+    for j in range(1,i):
+        print('◆', end = ' ')           
+    print()
+
+for i in range(0, 10):
+    print(i)
