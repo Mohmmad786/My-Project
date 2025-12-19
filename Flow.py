4c5457a6029072663fc26e47690f049067a0cc54
Mohmmad07861 <maknojiyamohmmad786@gmail.com>
2025-12-19 20:47:08 +0530

    Refactor patterns and add increase pattern

--- Flow.py
+++ Flow.py
@@ -1,14 +1,23 @@
-# 1 to 5 & 1
-for i in range(6,1,-1 ):  # 1,6
-    for j in range(1, i):   # (1,i+1)
+# Decrease Pattern
+for i in range(6,1,-1):  
+    for j in range(1, i):   
         print(j, end = ' ')
     print()
 
-#pattern 02 
+# Increase Pattern
+for i in range(1,6):
+    for j in range(1, i+1):
+        print(j, end = ' ')
+    print()                
+
+# pattern 02 
 for i in range(6,1,-1):             
     for j in range(1,i):
         print('◆', end = ' ')           
     print()
 
+# 0 ; 10 range
 for i in range(0, 10):
     print(i)
+
+
