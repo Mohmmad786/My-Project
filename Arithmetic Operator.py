38c71b077afd599fc643d06245df750fd55b77f8
Mohmmad07861 <maknojiyamohmmad786@gmail.com>
2025-12-19 20:51:11 +0530

    Expand Arithmetic Operator with more operations
    Added operations for subtraction, multiplication, and division.

--- Arithmetic Operator.py
+++ Arithmetic Operator.py
@@ -1,9 +1,29 @@
+# addition
 a = 5
 b = 2
 sum = (a+b)
 print (sum)
 
+# Reminder
 a=10
 b=5
-module = (a/b)
+module = (a%b)
 print (module)
+
+# Multiple
+a=6
+b=7
+multiple =(a*b)
+print (multiple) 
+
+#Subtract
+a=26
+b=13
+subtract =(a-b)
+print(subtract)
+
+# division
+a=10
+b=50
+divide=(a/b)
+print(divide)
